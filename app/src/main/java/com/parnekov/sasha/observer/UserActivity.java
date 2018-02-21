1a861f07b210a7ec3fe74202e3de8ec35bae1853
Sasha Parnekov <oleksandr.parnekov@gmail.com>
2018-02-21 12:39:13 +0200

    add Butterknife library and initialize variables

--- app/src/main/java/com/parnekov/sasha/observer/UserActivity.java
+++ app/src/main/java/com/parnekov/sasha/observer/UserActivity.java
@@ -2,16 +2,26 @@ package com.parnekov.sasha.observer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import java.util.Observable;
 import java.util.Observer;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class UserActivity extends AppCompatActivity implements Observer {
+    @BindView(R.id.tv_name)
+    TextView mTextViewName;
+
+    @BindView(R.id.tv_age)
+    TextView mTextViewAge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
     @Override
