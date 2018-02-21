c2ed0597e98723a91f3db409ebb14cbf5aebb217
Sasha Parnekov <oleksandr.parnekov@gmail.com>
2018-02-21 12:25:50 +0200

    implement Observer interface

--- app/src/main/java/com/parnekov/sasha/observer/UserActivity.java
+++ app/src/main/java/com/parnekov/sasha/observer/UserActivity.java
@@ -3,11 +3,19 @@ package com.parnekov.sasha.observer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Observable;
+import java.util.Observer;
+
+public class UserActivity extends AppCompatActivity implements Observer {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void update(Observable observable, Object o) {
+        
     }
 }
