123da7f5004aebd1098ee2a3da819b267413894f
Sasha Parnekov <oleksandr.parnekov@gmail.com>
2018-02-21 12:18:21 +0200

    add UserDataRepository class extends Observable

---
+++ app/src/main/java/com/parnekov/sasha/observer/UserDataRepository.java
@@ -0,0 +1,45 @@
+package com.parnekov.sasha.observer;
+
+import android.os.Handler;
+
+import java.util.Observable;
+
+
+public class UserDataRepository extends Observable {
+    private String mName;
+    private int mAge;
+
+    private static UserDataRepository INSTANCE = null;
+
+    private UserDataRepository() {
+        getNewDataFromRemote();
+    }
+
+
+    // Simulate network
+    private void getNewDataFromRemote() {
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                setUserData("Parniekov Sasha", 28);
+            }
+        }, 10000);
+    }
+
+    public void setUserData(String fullName, int age) {
+        mName = fullName;
+        mAge = age;
+        setChanged();
+        notifyObservers();
+    }
+
+    public String getFullName() {
+        return mName;
+    }
+
+    public int getAge() {
+        return mAge;
+    }
+
+}
